f9c506028be89bdcfbbff47cf798161e7137b8f0
Benedict Matthew Arzola <benedictmatthewaarzola@iskolarngbayan.pup.edu.ph>
2023-07-07 17:49:12 +0800

    Add building

---
+++ lib/add_building.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class AddBuilding extends StatefulWidget {
+  const AddBuilding({Key? key}) : super(key: key);
+
+  @override
+  _AddBuildingState createState() => _AddBuildingState();
+}
+
+class _AddBuildingState extends State<AddBuilding> {
+  int _selectedIndex = 1;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 7.0, left: 5.0),
+              child: Image.asset(
+                'assets/puplogo.png',
+                width: 45,
+                height: 45,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10.0, left: 13.0),
+              child: const Text(
+                'PUP R.M.R.S',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF7E0001),
+                  fontWeight: FontWeight.w200,
+                  fontFamily: 'Poppins',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: kMainPrimaryColor,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            top: 10,
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Choose Building',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      // Handle the image click here
+                      print('Building A clicked');
+                    },
+                    child: Container(
+                      width: 300,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF5B0101), // Change the color here
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 270, // Adjust the width here
+                            height: 220, // Adjust the height here
+                            child: Image.asset(
+                              'assets/pupbuilding1.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Building A',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white, // Change the text color here
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      // Handle the image click here
+                      print('Building B clicked');
+                    },
+                    child: Container(
+                      width: 300,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF5B0101), // Change the color here
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 270, // Adjust the width here
+                            height: 220, // Adjust the height here
+                            child: Image.asset(
+                              'assets/pupbuilding2.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'Building B',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white, // Change the text color here
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex = 1,
+        onTap: _onItemTapped,
+        selectedItemColor: Color(0xFF7E0001), // Color of the selected icon and label
+        unselectedItemColor: Colors.grey, // Color of the unselected icons and labels
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold), // Style for the selected label
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal), // Style for the unselected labels
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
