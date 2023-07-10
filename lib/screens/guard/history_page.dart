cf53713c8390862871214d5a621c912aa679f84b
oDominicDTo <dominicdttanas@gmail.com>
2023-07-11 07:02:43 +0800

    history_page.dart: Correct card list styling, Fix code

--- lib/screens/guard/history_page.dart
+++ lib/screens/guard/history_page.dart
@@ -144,136 +144,140 @@ class _HistoryPageState extends State<HistoryPage> {
     ),
     ),
     ),
-      Positioned(
-        top: 150,
-        left: 10,
-        right: 10,
-        bottom: -1,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey.withOpacity(0.3),
-              width: 1,
-            ),
+    Positioned(
+    top: 150,
+    left: 10,
+    right: 10,
+    bottom: -1,
+    child: Container(
+    decoration: BoxDecoration(
+    color: Colors.white,
+    border: Border.all(
+    color: Colors.grey.withOpacity(0.3),
+    width: 1,
+    ),
+    ),
+    child: ListView.builder(
+    itemCount: rooms.length,
+    itemBuilder: (context, index) {
+    final room = rooms[index];
+    if (room.date.year == selectedDate.year &&
+    room.date.month == selectedDate.month &&
+    room.date.day == selectedDate.day) {
+      return Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-          child: ListView.builder(
-            itemCount: rooms.length,
-            itemBuilder: (context, index) {
-              final room = rooms[index];
-              return Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+          child: ClipPath(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(
+                    color: room.courseColor,
+                    width: 10,
+                  ),
                 ),
-                child: ClipPath(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border(
-                        bottom: BorderSide(
-                          color: room.courseColor,
-                          width: 10,
-                        ),
-                      ),
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      height: 110,
-                      child: Row(
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                height: 110,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Expanded(
-                            flex: 1,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '${room.roomName}',
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Poppins',
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Text(
-                                  '   ${room.subjectName}',
-                                  style: const TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                Text(
-                                  '   ${room.professorName}',
-                                  style: const TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                ),
-                              ],
+                          Text(
+                            '${room.roomName}',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Poppins',
                             ),
                           ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                '${room.course}',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: room.courseColor,
-                                  fontFamily: 'Poppins',
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 35),
-                              Stack(
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.symmetric(horizontal: 1),
-                                    height: 20,
-                                    width: 150,
-                                    decoration: BoxDecoration(
-                                      color: Colors.black,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                        '    ${room.initialTime} - ${room.finalTime}',
-                                        style: const TextStyle(
-                                          fontFamily: 'Poppins',
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.normal,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  const Positioned(
-                                    left: 6,
-                                    top: 2,
-                                    bottom: 10,
-                                    child: Icon(
-                                      Icons.access_time,
-                                      color: Colors.white,
-                                      size: 15,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                          const SizedBox(height: 10),
+                          Text(
+                            '   ${room.subjectName}',
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            '   ${room.professorName}',
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w300,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          '${room.course}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: room.courseColor,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 35),
+                        Stack(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 1),
+                              height: 20,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '    ${room.initialTime} - ${room.finalTime}',
+                                  style: const TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const Positioned(
+                              left: 6,
+                              top: 2,
+                              bottom: 10,
+                              child: Icon(
+                                Icons.access_time,
+                                color: Colors.white,
+                                size: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              );
-            },
-          ),
-        ),
-      ),
-
+              ),
+            ),
+          ));
+    } else {
+      return Container(); // Return an empty container for dates without any rooms
+    }
+    },
+    ),
+    ),
+    ),
     ],
     ),
     );
