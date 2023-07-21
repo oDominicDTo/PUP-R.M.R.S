d18bc5d648d88ebca78f56f89ca4974bf6e3669f
oDominicDTo <dominicdttanas@gmail.com>
2023-07-22 07:54:29 +0800

    Fix fetching and display of current user's reservations
    - Successfully implement fetching and displaying the list of reservations for the current user/professor

--- lib/screens/faculty/models/retrieve_reservation_model.dart
+++ lib/screens/faculty/models/retrieve_reservation_model.dart
@@ -2,14 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Reservation {
   final String id;
-  final String buildingId;
-  final String floorId;
-  final String professorId;
-  final Timestamp reservationDate;
-  final String roomId;
-  final String subjectId;
   final String? subjectName; // Use String? to handle nullable values
-  final String courseId;
   final String? courseName; // Use String? to handle nullable values
   final Timestamp initialTime;
   final Timestamp finalTime;
@@ -17,14 +10,7 @@ class Reservation {
 
   Reservation({
     required this.id,
-    required this.buildingId,
-    required this.floorId,
-    required this.professorId,
-    required this.reservationDate,
-    required this.roomId,
-    required this.subjectId,
     this.subjectName, // Make subjectName nullable
-    required this.courseId,
     this.courseName, // Make courseName nullable
     required this.initialTime,
     required this.finalTime,
@@ -35,14 +21,7 @@ class Reservation {
     final data = snapshot.data() as Map<String, dynamic>;
     return Reservation(
       id: snapshot.id,
-      buildingId: data['buildingId'],
-      floorId: data['floorId'],
-      professorId: data['professorId'],
-      reservationDate: data['reservationDate'],
-      roomId: data['roomId'],
-      subjectId: data['subjectId'],
       subjectName: data['subjectName'],
-      courseId: data['courseId'],
       courseName: data['courseName'],
       initialTime: data['initialTime'],
       finalTime: data['finalTime'],
