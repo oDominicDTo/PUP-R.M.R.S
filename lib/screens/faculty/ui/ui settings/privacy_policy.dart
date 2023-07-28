48353e29ca3f7484a17eae79fb0e1d323d289843
itsmedemmyy <demmyy.29@gmail.com>
2023-07-28 10:11:54 +0800

    Improved UI design

--- lib/screens/faculty/ui/ui settings/privacy_policy.dart
+++ lib/screens/faculty/ui/ui settings/privacy_policy.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:appdevelopment/constants.dart';
 
 class PrivacyPolicy extends StatefulWidget {
   @override
@@ -9,15 +8,14 @@ class PrivacyPolicy extends StatefulWidget {
 
 class _PrivacyPolicyState extends State<PrivacyPolicy> {
   final String privacyPolicy = '''
-Our team built the PUPBC Room Reservation app as a Free app. This SERVICE is provided by us at no cost and is intended for use to the Polytechnic University of the Philippines Biñan Campus.
+    Our team built the PUPBC Room Reservation app as a Free app. This SERVICE is provided by us at no cost and is intended for use to the Polytechnic University of the Philippines Biñan Campus.
 
-This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.
+    This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.
 
-If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
+    If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
 
-The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at PUPBC Room Reservation App unless otherwise defined in this Privacy Policy.
-
-''';
+    The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at PUPBC Room Reservation App unless otherwise defined in this Privacy Policy.
+  ''';
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +24,8 @@ The terms used in this Privacy Policy have the same meanings as in our Terms and
         title: Text('Privacy Policy'),
       ),
       body: Container(
-        color: kMainPrimaryColor,
-        padding: EdgeInsets.all(16.0),
+        color: Colors.white,
+        padding: EdgeInsets.all(35.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -59,9 +57,10 @@ The terms used in this Privacy Policy have the same meanings as in our Terms and
                     _buildTableOfContentsLink(
                       'LOG DATA\n',
                       'LOG DATA',
-                      'We want to inform users that whenever you use our Service, in a case of an error in the app we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.'     ),
+                      'We want to inform users that whenever you use our Service, in a case of an error in the app we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.',
+                    ),
                     _buildTableOfContentsLink('SERVICE PROVIDER\n',
-                      'SERVICE PROVIDER', '''We may employ third-party companies and individuals due to the following reasons:
+                        'SERVICE PROVIDER', '''We may employ third-party companies and individuals due to the following reasons:
 *   To facilitate our Service;
 *   To provide the Service on our behalf;
 *   To perform Service-related services; or
@@ -74,8 +73,8 @@ We want to inform users of this Service that these third parties have access to 
                       'We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.',
                     ),
                     _buildTableOfContentsLink('CHANGES TO PRIVACY POLICY\n',
-                      'CHANGES TO THIS PRIVACY POLICY',
-                      '''W'e may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.
+                        'CHANGES TO THIS PRIVACY POLICY',
+                        '''We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.
 
 This policy is effective as of 2023-07-10'''
                     ),
--- lib/screens/faculty/ui/ui settings/terms_and_conditions.dart
+++ lib/screens/faculty/ui/ui settings/terms_and_conditions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:appdevelopment/constants.dart';
 
 class TermsandConditions extends StatefulWidget {
   @override
@@ -11,29 +10,29 @@ class _TermsandConditionsState extends State<TermsandConditions> {
   final String termsAndConditionsText = '''
 By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages or make derivative versions. The app itself, and all the trademarks, copyright, database rights, and other intellectual property rights related to it, still belong to us .
 
-Oue team is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.
+Our team is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.
 
-The PUPBC Room Reservation app stores and processes personal data that you have provided to us, to provide my Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the PUPBC Room Reservation app won’t work properly or at all.
+The PUPBC Room Reservation app stores and processes personal data that you have provided to us, to provide my Service. It’s your responsibility to keep your phone and access to the app secure. We, therefore, recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features, and it could mean that the PUPBC Room Reservation app won’t work properly or at all.
 
-You should be aware that there are certain things that our team will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi or provided by your mobile network provider, but our team cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.
+You should be aware that there are certain things that our team will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi or provided by your mobile network provider, but our team cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowances left.
 
-If you’re using the app outside of an area with Wi-Fi, you should remember that the terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third-party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.
+If you’re using the app outside of an area with Wi-Fi, you should remember that the terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app or other third-party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e., region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.
 
-Along the same lines, we cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, our team cannot accept responsibility.
+Along the same lines, we cannot always take responsibility for the way you use the app, i.e., You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, our team cannot accept responsibility.
 
-With respect to our  responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavor to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. We accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.
+With respect to our responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavor to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. We accept no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.
 
-At some point, we may wish to update the app. The app is currently available on Android – the requirements for the system(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. Our team does not promise that it will always update the app so that it is relevant to you and/or works with the Android version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.
+At some point, we may wish to update the app. The app is currently available on Android – the requirements for the system (and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. Our team does not promise that it will always update the app so that it is relevant to you and/or works with the Android version that you have installed on your device. However, you promise to always accept updates to the application when offered to you. We may also wish to stop providing the app and may terminate the use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.
 ''';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Term\'s and Conditions'),
+        title: Text('Terms and Conditions'),
       ),
       body: Container(
-        color: kMainPrimaryColor,
+        color: Colors.white,
         padding: EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
@@ -43,7 +42,7 @@ At some point, we may wish to update the app. The app is currently available on 
                 'AGREEMENT TO OUR LEGAL TERMS',
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 20,
+                  fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -54,7 +53,7 @@ At some point, we may wish to update the app. The app is currently available on 
                   text: termsAndConditionsText,
                   style: TextStyle(
                     color: Colors.black,
-                    fontSize: 15,
+                    fontSize: 16,
                   ),
                   children: [
                     TextSpan(
@@ -66,11 +65,12 @@ At some point, we may wish to update the app. The app is currently available on 
                     _buildTableOfContentsLink(
                       'CHANGES TO TERMS AND CONDITIONS\n',
                       'CHANGES TO THIS TERMS AND CONDITIONS',
-                    '''We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page.
+                      '''We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page.
 
 These terms and conditions are effective as of 2023-07-10''',
                     ),
-                    _buildTableOfContentsLink('CONTACT US\n',
+                    _buildTableOfContentsLink(
+                      'CONTACT US\n',
                       'CONTACT US',
                       'If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at OurTeamsG2@gmail.com.',
                     ),
@@ -108,7 +108,10 @@ These terms and conditions are effective as of 2023-07-10''',
         return AlertDialog(
           title: Text(sectionTitle),
           content: SingleChildScrollView(
-            child: Text(sectionText),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(sectionText),
+            ),
           ),
           actions: [
             TextButton(
